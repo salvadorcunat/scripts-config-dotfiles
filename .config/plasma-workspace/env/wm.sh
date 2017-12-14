4f5511f8061f67039d2f5dfd1060fbf7b7110493
Salvador Cuñat <salvador.cunat@gmail.com>
2017-12-14 22:33:05 +0100

    Add some debian .config files

---
+++ .config/plasma-workspace/env/wm.sh
@@ -0,0 +1,7 @@
+# Disable KWin and use i3gaps as WM
+export KDEWM="$HOME/.i3/i3-plasma"
+
+# Compositor (Animations, Shadows, Transparency)
+# xcompmgr -C
+# call compton from i3 config
+#compton -cCFb --backend glx --vsync opengl --config $HOME/.config/compton/compton.conf
